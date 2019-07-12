729414117fa7fbaeee4521eed52217ad0623d5a5
Keith <kayzee@live.com>
2019-07-11 21:43:23 -1000

    comments deleted

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -1,15 +1,31 @@
 /**
- * Returns a string representation of a 2-dimensional data structure 
- * @param {number} depth 
- * @param {number} [width=depth] 
+ * Returns a string representation of a 2-dimensional data structure
+ * @param {number} depth
+ * @param {number} [width=depth]
  * @returns {string}
  */
-function nestedForLoops (depth, width = depth) {
+function nestedForLoops(depth, width = depth) {
   let result = "\n";
+  let comma = `, `;
 
-  // Write code here
-
-};
+  for (let vertical = 0; vertical < depth; vertical++) {
+    for (let horizontal = 0; horizontal < width; horizontal++) {
+      let item = `{x:${horizontal}, y:${vertical}}`;
+      result += item;
+      if (horizontal === width - 1) {
+      } else {
+        result += comma;
+      }
+    }
+    result += `\n`;
+  }
+  return result;
+}
 
 // To see your console output outside the tests add function calls here.
-// console.log(nestedForLoops(4));
+
+console.log(nestedForLoops(3));
+
+console.log(nestedForLoops(10, 5));
+
+console.log(nestedForLoops(5, 10));
